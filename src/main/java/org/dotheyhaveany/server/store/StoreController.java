84ebfcf772cbf96f889b599291c5ac30d50cc6a9
Jon Chambers <jon.chambers@gmail.com>
2020-04-07 18:41:27 -0400

    Externalized strings.

--- src/main/java/org/dotheyhaveany/server/store/StoreController.java
+++ src/main/java/org/dotheyhaveany/server/store/StoreController.java
@@ -1,5 +1,6 @@
 package org.dotheyhaveany.server.store;
 
+import org.dotheyhaveany.server.report.ShoppingReportService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +16,22 @@ import java.util.Map;
 @RequestMapping("/store")
 public class StoreController {
 
+    private final ShoppingReportService shoppingReportService;
     private final StoreService storeService;
 
-    public StoreController(final StoreService storeService) {
+    public StoreController(final ShoppingReportService shoppingReportService, final StoreService storeService) {
+        this.shoppingReportService = shoppingReportService;
         this.storeService = storeService;
     }
 
     @GetMapping("/{storeId}")
     public ModelAndView get(@PathVariable final int storeId) {
+        final Store store = storeService.findById(storeId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         final Map<String, Object> model = new HashMap<>();
-        model.put("store", storeService.findById(storeId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+        model.put("store", store);
+
+        shoppingReportService.getInventoryReport(store).ifPresent(report -> model.put("inventoryReport", report));
 
         return new ModelAndView("store", model);
     }
